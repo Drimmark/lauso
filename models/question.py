aeb96f693036af9c2f321ee1d6d38486e5e2436a
Drimmark <axel.bc099@gmail.com>
2016-01-16 17:21:13 +0100

    question

--- models/question.py
+++ models/question.py
@@ -1,6 +1,11 @@
 from models.basicmodel import db
 
 class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=1)
+    cardinal = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    required = db.Column(db.Boolean, default=True, nullable=False)
+    survey = db.Column(db.Integer, db.ForeignKey('survey.id'), nullable=False)
 
     def __init__(self):
         pass
