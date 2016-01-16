729a4575448b9874c4c8bf2af05e835c05595fcb
root <root@Tsuki>
2016-01-16 17:43:17 +0100

    survey.py finished

--- models/survey.py
+++ models/survey.py
@@ -1,15 +1,22 @@
 from models.basicmodel import db
 
 class Survey(db.Model):
-	survey_id = db.Column(db.Integer, primary_key=True, autoincrement=1)
+	id = db.Column(db.Integer, primary_key=True, autoincrement=1)
 	code = db.Column(db.String(50), index=True)
 	name = db.Column(db.String(100), nullable = False)
-	owner = db.Column(db.Integer, nullable = False) #Add reference
+	owner = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False) #Add reference
 	description = db.Column(db.Text, nullable = False)
+	confirmation = db.Column(db.Text, nullable = False)
 	init_date = db.Column(db.date, nullable = False)
 
     def __init__(self):
-        pass
+        self.id = id
+        self.code = code
+        self.name = name
+        self.owner = owner
+        self.description = description
+        self.confirmation = confirmation
+        self.init_date = init_date
 
     def __repr__(self):
-        pass
+        return self.name
