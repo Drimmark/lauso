895ef71022ca7fb4c3775fb48b21e87deb6301c6
axel.bcc099@gmail.com <drimmark>
2017-03-26 11:56:42 +0200

    add title and subtitle

--- models/box_question.py
+++ models/box_question.py
@@ -3,7 +3,8 @@ from models.basicmodel import db
 
 class BoxQuestion(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    content = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(500), nullable=False)
+    subtitle = db.Column(db.Text, nullable=False)
     cardinal = db.Column(db.Integer, nullable=False)
     survey = db.Column(db.Integer, db.ForeignKey('survey.id'), nullable=False)
     required = db.Column(db.Boolean, default=True)
@@ -12,8 +13,9 @@ class BoxQuestion(db.Model):
     options = db.relationship('Option', backref='box_question', lazy='select')
     answers = db.relationship('BoxAnswer', backref='box_question', lazy='select')
 
-    def __init__(self, content, cardinal, survey, required=None, radio=None):
-        self.content = content
+    def __init__(self, title, subtitle, cardinal, survey, required=None, radio=None):
+        self.title = title
+        self.subtitle = subtitle
         self.cardinal = cardinal
         self.survey = survey
         self.required = required
--- models/question.py
+++ models/question.py
@@ -3,14 +3,18 @@ from models.basicmodel import db
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(500), nullable=False)
+    subtitle = db.Column(db.Text, nullable=False)
     cardinal = db.Column(db.Integer, nullable=False)
-    content = db.Column(db.Text, nullable=False)
     required = db.Column(db.Boolean, default=True, nullable=False)
     survey = db.Column(db.Integer, db.ForeignKey('survey.id'), nullable=False)
 
-    def __init__(self, cardinal, content, required, survey):
+    answers = db.relationship('Answer', backref='question', lazy='select')
+
+    def __init__(self, title, subtitle, cardinal, required, survey):
+        self.title = title
+        self.subtitle = subtitle
         self.cardinal = cardinal
-        self.content = content
         self.required = required
         self.survey = survey
 
