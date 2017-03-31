75b1eafae1625c63e278cb09fe7c007b18fd0131
axel.bcc099@gmail.com <drimmark>
2017-03-31 19:24:26 +0200

    autogenerate code

--- models/survey.py
+++ models/survey.py
@@ -1,11 +1,13 @@
 import datetime
+import hashlib
+import uuid
 
 from models.basicmodel import db
 
 
 class Survey(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    code = db.Column(db.String(50), nullable=False, index=True)
+    code = db.Column(db.String(50), index=True)
     name = db.Column(db.String(100), nullable=False)
     # owner is a reference to nia
     owner = db.Column(db.Integer, nullable=False)
@@ -13,13 +15,19 @@ class Survey(db.Model):
     confirmation = db.Column(db.Text, nullable=False)
     init_date = db.Column(db.Date, default=datetime.datetime.utcnow)
 
-    def __init__(self, code, name, owner, description, confirmation, init_date=None):
-        self.code = code
+    def __init__(self, name, owner, description, confirmation, init_date=None):
         self.name = name
         self.owner = owner
         self.description = description
         self.confirmation = confirmation
         self.init_date = init_date
+        self.code = self.generate_code()
 
     def __repr__(self):
         return str(self.__dict__)
+
+    def generate_code(self):
+        before_code = str(datetime.datetime.utcnow().strftime('%B%d%Y%H%M%S')) + str(self.owner) + str(uuid.uuid4().hex)
+        return str(hashlib.sha256(before_code.encode('utf-8')).hexdigest())[:50]
+
+
